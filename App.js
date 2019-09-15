e6842c4f6441e3b4ab62e20685e82d56f86946eb
huanggengzhong <915835528@qq.com>
2019-09-15 22:56:05 +0800

    初始化

---
+++ App.js
@@ -0,0 +1,22 @@
+
+
+import React, {Fragment} from 'react';
+import {
+  View,
+  Text,
+ 
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <Fragment>
+     <Text>哈哈3haha</Text>
+    </Fragment>
+  );
+};
+
+
+
+export default App;
